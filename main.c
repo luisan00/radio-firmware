8a2848f55eee248bbc0682a033883dd38fa83bf6
luisan00 <luisan00@hotmail.com>
2021-09-20 15:39:15 +0200

    update link-local

--- main.c
+++ main.c
@@ -47,46 +47,52 @@ int main(void)
     gnrc_netif_t *ieee802154_netif = _find_netif(NETDEV_TYPE_IEEE802154);
 
     /* Join LL-MANET-Routers multicast group */
-    if (manet_netif_ipv6_group_join(ieee802154_netif) < 0) {
+    if (manet_netif_ipv6_group_join(ieee802154_netif) < 0)
+    {
         printf("Couldn't join MANET mcast group\n");
     }
 
     /* Add global address */
     eui64_t iid;
-    if (gnrc_netif_ipv6_get_iid(ieee802154_netif, &iid) == sizeof(uint64_t)) {
-        ipv6_addr_t addr = {{ 0x20, 0x01, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00,
-                              0x00, 0x00, 0x00, 0x00 }};
+    if (gnrc_netif_ipv6_get_iid(ieee802154_netif, &iid) == sizeof(uint64_t))
+    {
+        ipv6_addr_t addr = {{0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02}};
         ipv6_addr_init_iid(&addr, (uint8_t *)&iid, 64);
 
-        if (gnrc_netif_ipv6_addr_add(ieee802154_netif, &addr, 128, 0) != sizeof(ipv6_addr_t)) {
+        if (gnrc_netif_ipv6_addr_add(ieee802154_netif, &addr, 128, 0) != sizeof(ipv6_addr_t))
+        {
             printf("Couln't setup global address\n");
         }
     }
 
     /* Initialize RFC5444 */
-    if (aodvv2_init(ieee802154_netif) < 0) {
+    if (aodvv2_init(ieee802154_netif) < 0)
+    {
         printf("Couldn't initialize RFC5444\n");
     }
 
 #if IS_USED(MODULE_VAINA)
     gnrc_netif_t *slipdev_netif = _find_netif(NETDEV_TYPE_SLIP);
     printf("found SLIP netif %d\n", slipdev_netif->pid);
-    if (slipdev_netif == NULL) {
+    if (slipdev_netif == NULL)
+    {
         printf("VAINA needs a wired interface (SLIP) to work!\n");
     }
-    else {
+    else
+    {
         ipv6_addr_t addr;
-        if (ipv6_addr_from_str(&addr, CONFIG_SLIP_LOCAL_ADDR) == NULL) {
+        if (ipv6_addr_from_str(&addr, CONFIG_SLIP_LOCAL_ADDR) == NULL)
+        {
             printf("Malformed SLIP local address, please verify it!\n");
         }
 
-        if (gnrc_netif_ipv6_addr_add(slipdev_netif, &addr, 128, 0) != sizeof(ipv6_addr_t)) {
+        if (gnrc_netif_ipv6_addr_add(slipdev_netif, &addr, 128, 0) != sizeof(ipv6_addr_t))
+        {
             printf("Couldn't setup SLIP local address\n");
         }
 
-        if (vaina_init(slipdev_netif) < 0) {
+        if (vaina_init(slipdev_netif) < 0)
+        {
             printf("Couldn't initialize VAINA\n");
         }
     }
@@ -121,9 +127,12 @@ static gnrc_netif_t *_find_netif(uint16_t nettype)
     gnrc_netif_t *netif = NULL;
     for (netif = gnrc_netif_iter(netif);
          netif != NULL;
-         netif = gnrc_netif_iter(netif)) {
-        if (gnrc_netif_get_from_netdev(netif, &opt) == sizeof(uint16_t)) {
-            if (device_type == nettype) {
+         netif = gnrc_netif_iter(netif))
+    {
+        if (gnrc_netif_get_from_netdev(netif, &opt) == sizeof(uint16_t))
+        {
+            if (device_type == nettype)
+            {
                 return netif;
             }
         }
